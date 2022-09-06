94bce4d5b3314458447a154a7cde252a1bd2cd2d
Lionel Besson <lionel.besson@gmail.com>
2022-09-06 14:35:31 +0200

    use starter level for k8s

--- exoscale/demo/k8s.tf
+++ exoscale/demo/k8s.tf
@@ -1,6 +1,7 @@
 resource "exoscale_sks_cluster" "c2c" {
-  zone = var.default_zone
-  name = "${var.environment}-cluster"
+  zone          = var.default_zone
+  name          = "${var.environment}-cluster"
+  service_level = "starter"
 }
 
 output "my_sks_cluster_endpoint" {
