77bba0d9f6fbe5e81febf3ea7f13345f03bb876e
Lionel Besson <lionel.besson@gmail.com>
2022-09-06 14:15:17 +0200

    Reorganize folders

---
+++ exoscale/production/variables.tf
@@ -0,0 +1,2 @@
+variable "exoscale_api_key" { type = string }
+variable "exoscale_api_secret" { type = string }
---
+++ modules/README.md
@@ -0,0 +1,3 @@
+# Modules
+
+Define here the terraform modules to be used in different environments
