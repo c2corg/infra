e34ffabe1ee2e864db1b36c6c6fdef94e9a53041
Lionel Besson <lionel.besson@gmail.com>
2022-09-06 14:56:22 +0200

    security groups for sks node pool

--- exoscale/demo/k8s.tf
+++ exoscale/demo/k8s.tf
@@ -4,6 +4,36 @@ resource "exoscale_sks_cluster" "c2c" {
   service_level = "starter"
 }
 
-output "my_sks_cluster_endpoint" {
-  value = exoscale_sks_cluster.c2c.endpoint
+resource "exoscale_security_group" "k8s" {
+  name = "${var.environment}-cluster-security-group"
+}
+
+resource "exoscale_security_group_rule" "nodeport_services" {
+  security_group_id = exoscale_security_group.k8s.id
+  description       = "NodePort services"
+  type              = "INGRESS"
+  protocol          = "TCP"
+  cidr              = "0.0.0.0/0"
+  start_port        = 30000
+  end_port          = 32767
+}
+
+resource "exoscale_security_group_rule" "kubelet" {
+  security_group_id = exoscale_security_group.k8s.id
+  description       = "SKS kubelet"
+  type              = "INGRESS"
+  protocol          = "TCP"
+  cidr              = "0.0.0.0/0"
+  start_port        = 10250
+  end_port          = 10250
+}
+
+resource "exoscale_security_group_rule" "calico_traffic" {
+  security_group_id = exoscale_security_group.k8s.id
+  description       = "Calico traffic"
+  type              = "INGRESS"
+  protocol          = "UDP"
+  cidr              = "0.0.0.0/0"
+  start_port        = 4789
+  end_port          = 4789
 }
