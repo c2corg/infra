1e4c1ae32aa11a53deb9878820f03010468a5232
Lionel Besson <lionel.besson@gmail.com>
2022-09-08 15:43:44 +0200

    s3

--- exoscale/demo/main.tf
+++ exoscale/demo/main.tf
@@ -3,6 +3,13 @@ provider "exoscale" {
   secret = var.exoscale_api_secret
 }
 
+provider "kubernetes" {
+  host                   = yamldecode(exoscale_sks_kubeconfig.c2c.kubeconfig).clusters[0].cluster.server
+  cluster_ca_certificate = base64decode(yamldecode(exoscale_sks_kubeconfig.c2c.kubeconfig).clusters[0].cluster.certificate-authority-data)
+  client_certificate     = base64decode(yamldecode(exoscale_sks_kubeconfig.c2c.kubeconfig).users[0].user.client-certificate-data)
+  client_key             = base64decode(yamldecode(exoscale_sks_kubeconfig.c2c.kubeconfig).users[0].user.client-key-data)
+}
+
 provider "helm" {
   kubernetes {
     host                   = yamldecode(exoscale_sks_kubeconfig.c2c.kubeconfig).clusters[0].cluster.server
@@ -10,4 +17,21 @@ provider "helm" {
     client_certificate     = base64decode(yamldecode(exoscale_sks_kubeconfig.c2c.kubeconfig).users[0].user.client-certificate-data)
     client_key             = base64decode(yamldecode(exoscale_sks_kubeconfig.c2c.kubeconfig).users[0].user.client-key-data)
   }
+}
+
+# Use aws provider to manage exoscale object storage
+provider "aws" {
+  endpoints {
+    s3 = "https://sos-${var.default_zone}.exo.io"
+  }
+  region     = var.default_zone
+  access_key = var.exoscale_api_key
+  secret_key = var.exoscale_api_secret
+  # Skip AWS validations
+  skip_credentials_validation = true
+  skip_get_ec2_platforms      = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+
 }
--- exoscale/demo/versions.tf
+++ exoscale/demo/versions.tf
@@ -4,6 +4,18 @@ terraform {
       version = ">= 0.40.1"
       source  = "exoscale/exoscale"
     }
+    aws = {
+      version = ">= 4.29.0"
+      source  = "hashicorp/aws"
+    }
+    helm = {
+      version = ">= 2.6.0"
+      source  = "hashicorp/helm"
+    }
+    kubernetes = {
+      version = ">= 2.13.1"
+      source  = "hashicorp/kubernetes"
+    }
   }
   required_version = ">= 1.0"
 }
