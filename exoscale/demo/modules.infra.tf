d9791b216947079bee813ba50d21cd9283150286
Lionel Besson <lionel.besson@gmail.com>
2022-09-15 13:41:21 +0200

    enable metrics for haproxy and cert-manager

--- exoscale/demo/modules.infra.tf
+++ exoscale/demo/modules.infra.tf
@@ -1,18 +1,17 @@
 module "haproxy" {
-  source         = "../../modules/haproxy-ingress"
-  chart_version  = var.haproxy_ingress_version
-  replica_count  = 1
-  enable_metrics = false
+  source        = "../../modules/haproxy-ingress"
+  chart_version = var.haproxy_ingress_version
+  replica_count = 1
 }
 
 module "cert-manager" {
-  source         = "../../modules/cert-manager"
-  chart_version  = var.cert_manager_version
-  enable_metrics = false
+  source        = "../../modules/cert-manager"
+  chart_version = var.cert_manager_version
 }
 
 module "kube-prometheus-stack" {
   source            = "../../modules/kube-prometheus-stack"
   chart_version     = var.kube_prometheus_stack_version
   grafana_admin_pwd = var.grafana_admin_pwd
+  enable_ingress    = false
 }
