f6213de96240cbb392bbe6137b7074d8f4f91841
Lionel Besson <lionel.besson@gmail.com>
2022-11-15 13:43:49 +0100

    Do not use env for setting version (docker image handles it)

--- modules/c2c_images/main.tf
+++ modules/c2c_images/main.tf
@@ -115,7 +115,6 @@ resource "kubernetes_config_map" "c2c-images-config-map" {
   data = {
     DEBUG                      = "c2c_images:*"
     NODE_ENV                   = var.environment
-    VERSION                    = var.image_tag
     METRICS_PORT               = 8081
     SERVICE_PORT               = 8080
     TEMP_FOLDER                = var.temp_folder
