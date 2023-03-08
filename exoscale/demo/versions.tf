e57d38b48ed7d24255db2cd24af5926b213fab37
Lionel Besson <lionel.besson@gmail.com>
2023-03-08 17:45:36 +0100

    typo

--- exoscale/demo/versions.tf
+++ exoscale/demo/versions.tf
@@ -1,10 +1,4 @@
 terraform {
-  cloud {
-    organization = "camptocamp-org"
-    workspaces {
-      tags = ["demo"]
-    }
-  }
   required_providers {
     exoscale = {
       version = "= 0.46.0"
